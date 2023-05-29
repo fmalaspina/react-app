7b648eff5e638e4ba08d5c7ed7cfd6b0bbeb0084
Administrator <f_malaspina@hotmail.com>
2023-05-29 06:25:48 -0300

    added handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import ListGroup from './components/ListGroup';
+import ListGroup from "./components/ListGroup";
+
 function App() {
-  return <div><ListGroup /></div>
+  const items = ["New York", "San Francisco", "Tokio", "London", "Paris"];
+  const handleSelectItem = (item: string): void => {console.log(item) };
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>
+  );
 }
 
 export default App;
